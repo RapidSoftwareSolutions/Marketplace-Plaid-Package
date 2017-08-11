c39f7c980a1eb7e3c46b29a9484717717f4f3696
Denis Raygorodskiy <raygorodskij@rapidapi.com>
2017-08-11 17:21:04 +0300

    update to production mode

--- src/settings.php
+++ src/settings.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'settings' => [
-        'baseUrl' => 'https://sandbox.plaid.com',
+        'baseUrl' => 'https://production.plaid.com',
         'clientId' => '59845ffcbdc6a4059887f2d2',
         'publicKey' => '4e4efc9538a3810003c54cb62bffa2',
         'secret' => '0a3cc73a6836ab5db07247b04638a7',
